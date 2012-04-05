2b2b4ff48aa88eb92cb39779be0d9bd96ef10414
Alex Hornung <ahornung@gmail.com>
2012-04-05 17:51:28 +0100

    use relative paths for text! requirejs

--- public/js/app/appns.js
+++ public/js/app/appns.js
@@ -1,4 +1,4 @@
-define(['jquery', 'text', 'text!/tmpl/all.templates'], function($, templates) {
+define(['jquery', 'text!../tmpl/all.templates'], function($, templates) {
   $("body").append(templates);
 
   App = {};
