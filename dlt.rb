a23c1b8959642fa6e8f02037ae048783a7420628
Alex Hornung <ahornung@gmail.com>
2012-04-13 21:20:52 +0100

    add public_folder, too

--- dlt.rb
+++ dlt.rb
@@ -36,6 +36,7 @@ set :static_cache_control, [:public, :max_age => 43200] # 12 hours
 if defined? @config['optimized'] and @config['optimized']
   puts "Using optimized static files"
   set :public, File.dirname(__FILE__) + '/public/build'
+  set :public_folder, File.dirname(__FILE__) + '/public/build'
 end
 
 configure do
