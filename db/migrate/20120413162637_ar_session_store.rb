0c635bb72fe500cecb6a08b19df6824becaf5909
Alex Hornung <ahornung@gmail.com>
2012-04-13 17:37:57 +0100

    fix ar session store migration

--- db/migrate/20120413162637_ar_session_store.rb
+++ db/migrate/20120413162637_ar_session_store.rb
@@ -1,8 +1,8 @@
 class ArSessionStore < ActiveRecord::Migration
   def self.up
     create_table :sessions do |t|
-      t.text :session_id
-      t.text :data
+      t.string :session_id
+      t.text   :data
     end
 
     add_index :sessions, :session_id, :unique => true
