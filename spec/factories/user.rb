86a9480b38177f8d8cfeda3591d6af7f001ee502
Alex Hornung <ahornung@gmail.com>
2012-06-12 08:18:02 +0100

    more bits and bobs of factories

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,7 +1,19 @@
 FactoryGirl.define do
   sequence(:email) { |n| "user-#{n}@example.com" }
+  sequence(:alias) { |n| "user#{n}" }
 
   factory :user do
+    sequence(:name) { |n| "John Doe #{n}" }
     email
+    alias
+    password "moo"
+  end
+
+  factory :project do
+    sequence(:name) { |n| "Project #{n}" }
+  end
+
+  factory :task do
+    sequence(:summary) { |n| "Task #{n}" }
   end
 end
